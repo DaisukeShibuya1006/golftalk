2f91b7f7743a08bdc6edb82d9d36064ce17b3da1
DaisukeShibuya1006 <shibudai1006@yahoo.co.jp>
2019-10-08 15:02:16 +0900

    add columns
    カラムを複数追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_043458) do
+ActiveRecord::Schema.define(version: 2019_10_07_154228) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2019_10_05_043458) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "image"
+    t.string "uid"
+    t.string "username"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
